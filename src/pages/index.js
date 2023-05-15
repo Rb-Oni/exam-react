f17ea145401ab449384c374837d09bae6cc7d40a
Rb-Oni <robin.falck57@gmail.com>
2023-05-15 22:49:53 +0200

    tailwind fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,17 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Exam REACT</title>
-        <meta name="description" content="Exam REACT, FALCK Robin RPI-DEV 2023" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Hello</h1>
-      </main>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Exam REACT</title>
+				<meta name="description" content="Exam REACT, FALCK Robin RPI-DEV 2023"/>
+				<meta name="viewport" content="width=device-width, initial-scale=1"/>
+				<link rel="icon" href="/favicon.ico"/>
+			</Head>
+			<main>
+				<h1 className={"text-5xl"}>Hello</h1>
+			</main>
+		</>
+	)
 }
