db4ac5f40eb0d635355eb33c2a9e4edb300b00fc
Robin FALCK <robin.falck@diamontis.fr>
2023-05-16 14:14:29 +0200

    fetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,20 @@
 import Head from 'next/head'
+import {useEffect, useState} from 'react';
 
 export default function Home() {
+
+	const [name, setName] = useState([]);
+
+	useEffect(() => {
+		names()
+	}, [])
+
+	const names = async () => {
+		const responce = await fetch('http://localhost:3000/api/places');
+
+		setName(await responce.json())
+	}
+
 	return (
 		<>
 			<Head>
@@ -10,7 +24,25 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico"/>
 			</Head>
 			<main>
-				<h1 className={"text-5xl"}>Hello</h1>
+				<h1 className={'text-5xl'}>Hello</h1>
+				<section className={'my-10 px-4 grid grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-4'}>
+					{name.map((data) => {
+						return (
+							<a href={'#'} className={'mx-auto'} key={data.id}>
+								<img src={data.image}
+								     className={'opacity-90 hover:opacity-100 duration-150 w-60 rounded-lg'}
+								     alt="{data.image}"/>
+								<div className={'flex flex-col'}>
+									<div className={'flex justify-between'}>
+										<h2 className={'font-bold'}>{data.city.name}</h2>
+										<span className={'font-bold'}>{data.priceByNight}€</span>
+									</div>
+									<span>{data.name}</span>
+								</div>
+							</a>
+						)
+					})}
+				</section>
 			</main>
 		</>
 	)
