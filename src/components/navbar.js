bb87891f6811a08b66452dbf2fa34358d5920b98
Rb-Oni <robin.falck57@gmail.com>
2023-05-17 10:18:07 +0200

    details design

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -27,7 +27,7 @@ const Navbar = () => {
                     </nav>
                     <a href={'/user/update'}
                        className={router.pathname === '/user/update' ? 'justify-end text-custom text-2xl ease-in duration-150 md:mt-0 md:ml-4' : 'justify-end hover:text-custom focus:text-custom text-2xl ease-in duration-150 md:mt-0 md:ml-4'}>
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10 text-bluec" fill="none"
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none"
                              viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                             <path strokeLinecap="round" strokeLinejoin="round"
                                   d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/>
--- src/pages/[placeID].js
+++ src/pages/[placeID].js
@@ -3,31 +3,44 @@ import {useState, useEffect} from "react";
 import {useRouter} from 'next/router'
 
 export default function PlaceID() {
-	const router = useRouter();
-	const [place, setPlace] = useState('');
-	const {placeID} = router.query;
+    const router = useRouter();
+    const [place, setPlace] = useState('');
+    const {placeID} = router.query;
 
-	useEffect(() => {
-		fetch(`/api/places/${placeID}`)
-			.then((response) => response.json())
-			.then(data => setPlace(data));
-	}, [placeID]);
+    useEffect(() => {
+        fetch(`/api/places/${placeID}`)
+            .then((response) => response.json())
+            .then(data => setPlace(data));
+    }, [placeID]);
 
-	// J'ai essayé de display le nom de l'hôte et de la ville en vain alors que je récupère bien l'objet
-	console.log(place);
-	return (
-		<>
-			<Navbar></Navbar>
-			<main>
-				<section className={'my-10 px-4 container mx-auto'}>
-					<h1 className={'text-5xl font-bold'}>{place.name}</h1>
-					{/*<h2>{place.host.name}</h2>*/}
-					<div className={'flex my-4 gap-4'}>
-						<img src={place.image} alt={place.image}/>
-						<p>{place.description}</p>
-					</div>
-				</section>
-			</main>
-		</>
-	)
+    // J'ai essayé de display le nom de l'hôte et de la ville en vain alors que je récupère bien l'objet
+    console.log(place);
+    return (
+        <>
+            <Navbar></Navbar>
+            <main>
+                <section className={'my-10 px-4 container mx-auto'}>
+                    <h1 className={'text-5xl font-semibold'}>{place.name}</h1>
+                    <div className={'flex flex-row gap-8'}>
+                        <h2 className={'text-lg'}><span className={' font-semibold'}>{place.priceByNight}€</span> / by
+                            night</h2>
+                        {/*<span>{place.city.name}</span>*/}
+                        {/*<h2>{place.host.name}</h2>*/}
+                    </div>
+                    <div className={'flex my-4 gap-4'}>
+                        <img src={place.image} alt={place.image}/>
+                        <p>{place.description}</p>
+                    </div>
+                    <div className={'flex flex-row gap-8'}>
+                        <h3 className={'text-lg'}><span
+                            className={'font-semibold'}>{place.numberOfRooms}</span> {place.numberOfRooms > 1 ? "rooms" : "room"}
+                        </h3>
+                        <h3 className={'text-lg'}><span
+                            className={'font-semibold'}>{place.numberOfBathrooms}</span> {place.numberOfBathrooms > 1 ? "bathrooms" : "bathroom"}
+                        </h3>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
 }
